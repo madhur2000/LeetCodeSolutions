f5c9df437a4e4d2d02c8b6814233c0b2cac029d4
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-05-22 10:45:20 +0530

    Update #234 PalindromeLinkedList.java

--- #234 PalindromeLinkedList.java
+++ #234 PalindromeLinkedList.java
@@ -11,6 +11,8 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
+//         Approach-1: gives TLE [O(N^2)]
+        
 //         if(head == null || head.next == null)
 //             return true;
         
@@ -30,43 +32,116 @@ class Solution {
 //             return true;
 //         return false;
         
-        ArrayDeque<Integer> st = new ArrayDeque<>();
         
-        ListNode temp = head;
-        int length = 0;
         
-        while(temp != null){
-            length++;
-            temp = temp.next;
+//         Approach-2: Accepted [O(N)] (uses stack)
+        
+//         ArrayDeque<Integer> st = new ArrayDeque<>();
+        
+//         ListNode temp = head;
+//         int length = 0;
+        
+//         while(temp != null){
+//             length++;
+//             temp = temp.next;
+//         }
+        
+//         ListNode mid = head;
+        
+//         if((length & 1) != 0){
+//             for(int i=0; i<length/2; i++){
+//                 mid = mid.next;
+//             }
+//         }
+//         else{
+//             mid = null;
+//         }
+        
+        
+//         temp = head;
+        
+//         while(temp != null){
+//             if(temp != mid){
+//                 if(st.isEmpty() || st.peek() != temp.val){
+//                     st.push(temp.val);
+//                 }
+//                 else{
+//                     st.pop();
+//                 }
+//             }
+//             temp = temp.next;
+//         }
+        
+//         return (st.isEmpty()) ? true : false;
+     
+        
+//         Approach-3: reverse the list starting from mid
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            
+            slow = slow.next;
+            fast = fast.next.next;
+            
         }
         
-        ListNode mid = head;
+        ListNode head2 = slow.next;
+        slow.next = null;
         
-        if((length & 1) != 0){
-            for(int i=0; i<length/2; i++){
-                mid = mid.next;
+        ListNode head2_rev = reverseListIteratve(head2);
+        
+        while(head2_rev != null){
+            
+            if(head.val == head2_rev.val){
+                head = head.next;
+                head2_rev = head2_rev.next;
             }
-        }
-        else{
-            mid = null;
-        }
-        
-        
-        temp = head;
-        
-        while(temp != null){
-            if(temp != mid){
-                if(st.isEmpty() || st.peek() != temp.val){
-                    st.push(temp.val);
-                }
-                else{
-                    st.pop();
-                }
+            else{
+                return false;
             }
-            temp = temp.next;
+            
         }
         
-        return (st.isEmpty()) ? true : false;
-        
+        return true;
     }
+    
+    
+//     public static ListNode reverseListRecursive(ListNode head){
+        
+//         if(head == null || head.next == null)
+//             return head;
+        
+//         ListNode smallAns = reverseListRecursive(head.next);
+        
+//         ListNode smallAnsTail = head.next;
+        
+//         head.next = null;
+        
+//         smallAnsTail.next = head;
+        
+//         return smallAns;
+        
+//     }
+    
+    public static ListNode reverseListIteratve(ListNode head){
+        
+        if(head == null)
+            return null;
+        
+        ListNode prev = null, curr = head, next = head.next;
+        
+        while(curr != null){
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            if(next != null){
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+    
+    
 }
