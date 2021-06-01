c938cd7a256a0116ee01e71ff7a948595e191526
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-06-01 14:29:35 +0530

    Update #62 UniquePaths.java

--- #62 UniquePaths.java
+++ #62 UniquePaths.java
@@ -72,3 +72,74 @@ class Solution {
 //     }
     
 }
+
+
+// Revision Session: (much better code)
+
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+//         Approach-1: Recursion (gives TLE)
+        
+//         if(m == 1 || n == 1)
+//             return 1;
+        
+//         return(uniquePaths(m-1, n) + uniquePaths(m, n-1));
+        
+//         Approach-2: top-down DP
+//         int[][] storage = new int[m+1][n+1];
+        
+//         for(int[] row : storage){
+//             Arrays.fill(row, -1);
+//         }
+//         return helper(m, n, storage);
+        
+//         Approach-3: Bottom-up DP
+        
+        int[][] storage = new int[m+1][n+1];
+        
+        for(int i=0; i<=n; i++){
+            storage[0][i] = 0;
+        }
+        for(int j=0; j<=m; j++){
+            storage[j][0] = 0;
+        }
+        for(int i=1; i<=n; i++){
+            storage[1][i] = 1;
+        }
+        for(int i=1; i<=m; i++){
+            storage[i][1] = 1;
+        }
+        for(int i=2; i<=m; i++){
+            for(int j=2; j<=n; j++){
+                storage[i][j] = storage[i-1][j] + storage[i][j-1];
+            }
+        }
+        return storage[m][n];
+    }
+    
+//     public static int helper(int m, int n, int[][] storage){
+        
+//         if(m == 1 || n == 1)
+//             return 1;
+        
+//         if(storage[m][n] != -1)
+//             return storage[m][n];
+        
+//         int sa1 = helper(m-1, n, storage);
+//         storage[m-1][n] = sa1;
+        
+//         int sa2 = helper(m, n-1, storage);
+//         storage[m][n-1] = sa2;
+        
+//         storage[m][n] = sa1 + sa2;
+//         return storage[m][n];
+//     }
+    
+}
+
+
+
+
+
