eb0e1dbcb84812d3cc91a9a474259ee0d6b0494b
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-06-01 13:13:54 +0530

    Update #46 Permutations.java

--- #46 Permutations.java
+++ #46 Permutations.java
@@ -2,52 +2,38 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         
         if(nums.length == 1){
-            List<List<Integer>> result = new ArrayList<>();
-            // result = new ArrayList<>();
+            
+            List<List<Integer>> ans = new ArrayList<>();
             List<Integer> temp = new ArrayList<>();
             temp.add(nums[0]);
-            result.add(temp);
-            return result;
+            ans.add(temp);
+            return ans;
         }
-        // List<List<Integer>> al = new ArrayList<>();
-        // return al;
         
-        List<List<Integer>> result = new ArrayList<>();
-        
-        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         
         for(int i=0; i<nums.length; i++){
-            int[] currArr = getArrayLeavingI(nums, i);
-            // List<Integer> temp = new ArrayList<>();
-            // temp.add(nums[i]);
             
-            List<List<Integer>> temp2 = permute(currArr);
-            for(int j=0; j<temp2.size(); j++){
-                
-                List<Integer> temp = new ArrayList<>();
-                temp.add(nums[i]);
-                
-                for(int k=0; k<temp2.get(j).size(); k++){
-                    temp.add(temp2.get(j).get(k));
+            int[] temp = new int[nums.length-1];
+            int g=0;
+            for(int j=0; j<nums.length; j++){
+                if(j != i){
+                    temp[g++] = nums[j];
                 }
-                result.add(temp);
+            }
+            
+            List<List<Integer>> sa = permute(temp);
+            
+            for(int k=0; k<sa.size(); k++){
+                
+                ArrayList<Integer> curr = new ArrayList<>();
+                curr.add(nums[i]);
+                curr.addAll(sa.get(k));
+                ans.add(curr);
+                
             }
             
         }
-        
-        return result;
+        return ans;
     }
-    
-    public static int[] getArrayLeavingI(int[] nums, int i){
-        int[] result = new int[nums.length-1];
-        int g=0;
-        
-        for(int m=0; m<nums.length; m++){
-            if(m != i){
-                result[g++] = nums[m];
-            }
-        }
-        return result;
-    }
-    
 }
