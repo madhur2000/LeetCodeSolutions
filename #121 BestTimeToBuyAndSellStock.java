1e96e5a0542a9bcbb6305b02f0edb572a1bf37e8
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-06-02 16:18:18 +0530

    Update #121 BestTimeToBuyAndSellStock.java

--- #121 BestTimeToBuyAndSellStock.java
+++ #121 BestTimeToBuyAndSellStock.java
@@ -50,3 +50,45 @@ class Solution {
 //     }
     
 }
+
+
+
+
+
+
+// Revision Session:
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         if(prices == null || prices.length == 0)
+//             return 0;
+        
+//         int min = prices[0], max = prices[0];
+//         int ans = 0;
+        
+//         for(int i=1; i<prices.length; i++){
+//             if(prices[i] < min){
+//                 ans = Math.max(ans, max-min);
+//                 min = prices[i];
+//                 max = prices[i];
+//             }
+//             else if(prices[i] > max){
+//                 max = prices[i];
+//             }
+//         }
+//         return Math.max(ans, max-min);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
