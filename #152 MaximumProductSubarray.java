141d037bc384407df3ff3e9fa902d33c53cf5dd2
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-06-02 22:33:54 +0530

    Update #152 MaximumProductSubarray.java

--- #152 MaximumProductSubarray.java
+++ #152 MaximumProductSubarray.java
@@ -117,3 +117,90 @@ class Solution {
 //     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Revision Session:
+
+// class Solution {
+//     public int maxProduct(int[] nums) {
+        
+// //         int maxProduct = Integer.MIN_VALUE;
+        
+// //         for(int i=0; i<nums.length; i++){
+// //             for(int j=i; j<nums.length; j++){
+// //                 int currProduct = getProduct(nums, i, j);
+// //                 maxProduct = Math.max(maxProduct, currProduct);
+// //             }
+// //         }
+// //         return maxProduct;
+        
+        
+//         int[] min = new int[nums.length];
+//         min[0] = nums[0];
+//         int[] max = new int[nums.length];
+//         max[0] = nums[0];
+        
+//         int ans = max[0];
+        
+//         for(int i=1; i<nums.length; i++){
+            
+//             if(nums[i] == 0){
+//                 ans = Math.max(ans, max[i-1]);
+//                 max[i] = 1;
+//                 min[i] = 1;
+                
+//                 if(ans < 0)
+//                     ans = 0;
+                
+//                 continue;
+//             }
+            
+//             max[i] = Math.max(nums[i], Math.max((max[i-1] * nums[i]), (min[i-1] * nums[i])));
+            
+//             min[i] = Math.min(nums[i], Math.min((max[i-1] * nums[i]), (min[i-1] * nums[i])));
+            
+//             ans = Math.max(ans, max[i]);
+            
+//         }
+        
+//         return ans;
+        
+//     }
+    
+// //     public static int getProduct(int[] nums, int start, int end){
+        
+// //         int ans = 1;
+        
+// //         for(int i=start; i<=end; i++){
+// //             ans = ans * nums[i];
+// //         }
+// //         return ans;
+// //     }
+    
+// }
+
+
+
+
+
+
+
