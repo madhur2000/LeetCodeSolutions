b0c1305fe337adb0e2b1ab82359d637eb1977b2b
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-06-03 19:34:38 +0530

    Update #198 HouseRobber.java

--- #198 HouseRobber.java
+++ #198 HouseRobber.java
@@ -92,3 +92,116 @@ class Solution {
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Revision Session:
+
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         // return robRecursive(nums, 0);
+        
+//         // return robRecursive2(nums, nums.length-1);
+        
+// //         Approach-2: (top-down DP)
+        
+// //         int[] storage = new int[nums.length+1];
+// //         Arrays.fill(storage, -1);
+        
+// //         return robM(nums, nums.length-1, storage);
+        
+        
+        
+        
+// //         Approach-3: (Bottom-up DP)
+        
+//         if(nums == null || nums.length == 0)
+//             return 0;
+        
+//         if(nums.length == 1)
+//             return nums[0];
+        
+//         int[] storage = new int[nums.length];
+        
+//         storage[0] = nums[0];
+//         storage[1] = Math.max(nums[0], nums[1]);
+        
+        
+//         for(int i=2; i<storage.length; i++){
+            
+//             storage[i] = Math.max(storage[i-1], storage[i-2] + nums[i]);
+            
+//         }
+        
+//         return storage[storage.length-1];
+//     }
+    
+// //     public static int robRecursive(int[] nums, int start){
+// //         if(start >= nums.length)
+// //             return 0;
+        
+// //         int option1 = nums[start] + robRecursive(nums, start+2);
+        
+// //         int option2 = robRecursive(nums, start+1);
+        
+// //         return Math.max(option1, option2);
+// //     }
+    
+// //     public static int robRecursive2(int[] nums, int end){
+        
+// //         if(end < 0)
+// //             return 0;
+        
+// //         int option1 = robRecursive2(nums, end-1);
+// //         int option2 = robRecursive2(nums, end-2) + nums[end];
+        
+// //         return Math.max(option1, option2);
+        
+// //     }
+    
+// //     public static int robM(int[] nums, int end, int[] storage){
+        
+// //         if(end < 0)
+// //             return 0;
+        
+// //         if(end == 0){
+// //             storage[end] = nums[end];
+// //             return storage[end];
+// //         }
+        
+// //         if(storage[end] != -1){
+// //             return storage[end];
+// //         }
+        
+// //         int sa1 = robM(nums, end-1, storage);
+// //         int sa2 = robM(nums, end-2, storage);
+
+// //         storage[end-1] = sa1;
+// //         if(end-2 >= 0){
+// //             storage[end-2] = sa2;
+// //         }
+// //         storage[end] = Math.max(sa1, sa2 + nums[end]);
+// //         return storage[end];
+// //     }
+    
+// }
