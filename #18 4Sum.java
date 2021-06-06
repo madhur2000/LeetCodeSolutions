f76e5ef4d7d40748f3b59081ed7da9580697bd0b
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-06-06 23:01:32 +0530

    Update #18 4Sum.java

--- #18 4Sum.java
+++ #18 4Sum.java
@@ -28,59 +28,110 @@ class Solution {
 //         return ans;
         
         
-//         Approach-2: reducing 1 loop using binary search
+//         Approach-2: reducing 1 loop using binary search O((N^3)*logN)
+        
+//         Arrays.sort(nums);
+        
+//         HashSet<List<Integer>> ansSet = new HashSet<>();
+        
+//         List<List<Integer>> ans = new ArrayList<>();
+        
+//         int n = nums.length;
+        
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 for(int k=j+1; k<n; k++){
+                    
+//                     int num4 = target - nums[i] - nums[j] - nums[k];
+                    
+//                     boolean found = binarySearch(nums, num4, k+1);
+                    
+//                     if(found){
+//                         ansSet.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], num4)));
+//                     }
+                    
+//                 }
+//             }
+//         }
+        
+//         ans = new ArrayList<>(ansSet);
+//         return ans;
+        
+        
+        
+//         Approach-3: Two pointers + 'twoSum' or 'pairSum' approach
         
         Arrays.sort(nums);
         
-        HashSet<List<Integer>> ansSet = new HashSet<>();
-        
         List<List<Integer>> ans = new ArrayList<>();
         
-        int n = nums.length;
-        
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    
-                    int num4 = target - nums[i] - nums[j] - nums[k];
-                    
-                    boolean found = binarySearch(nums, num4, k+1);
-                    
-                    if(found){
-                        ansSet.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], num4)));
+        for(int i=0; i<nums.length;){
+            for(int j=i+1; j<nums.length;){
+                int target2 = target - nums[i] - nums[j];
+                int k=j+1;
+                int l=nums.length-1;
+                
+                while(k < l){
+                    if(nums[k] + nums[l] == target2){
+                        List<Integer> smallAns = new ArrayList<>();
+                        smallAns.add(nums[i]);
+                        smallAns.add(nums[j]);
+                        smallAns.add(nums[k]);
+                        smallAns.add(nums[l]);
+                        ans.add(smallAns);
+                        int temp = nums[k];
+                        while(k < nums.length && temp == nums[k]){
+                            k++;
+                        }
+                        temp = nums[l];
+                        while(l >= 0 && temp == nums[l]){
+                            l--;
+                        }
+                    } else if(nums[k] + nums[l] < target2){
+                        k++;
+                    } else{
+                        l--;
                     }
-                    
                 }
+                
+                int temp = nums[j];
+                while(j < nums.length && nums[j] == temp){
+                    j++;
+                }
+                
+            }
+            
+            int temp = nums[i];
+            while(i < nums.length && nums[i] == temp){
+                i++;
             }
         }
-        
-        ans = new ArrayList<>(ansSet);
         return ans;
         
     }
     
-    public static boolean binarySearch(int[] nums, int num, int start){
+//     public static boolean binarySearch(int[] nums, int num, int start){
         
-        // int start = k+1;
-        int end = nums.length-1;
+//         // int start = k+1;
+//         int end = nums.length-1;
         
-        while(start <= end){
+//         while(start <= end){
             
-            int mid = (start + end)/2;
+//             int mid = (start + end)/2;
             
-            if(num == nums[mid])
-                return true;
-            else if(num > nums[mid]){
+//             if(num == nums[mid])
+//                 return true;
+//             else if(num > nums[mid]){
                 
-                start = mid+1;
+//                 start = mid+1;
                 
-            }
-            else{
-                end = mid-1;
-            }
+//             }
+//             else{
+//                 end = mid-1;
+//             }
             
-        }
-        return false;
-    }
+//         }
+//         return false;
+//     }
     
 }
