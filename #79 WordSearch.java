5c0725dbea992bf063382198973ddf44562fb2ff
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-06-13 17:25:44 +0530

    Update #79 WordSearch.java

--- #79 WordSearch.java
+++ #79 WordSearch.java
@@ -4,11 +4,12 @@ class Solution {
         int rowCount = board.length;
         int colCount = board[0].length;
         
+        boolean[][] visited = new boolean[rowCount][colCount];
+        
         for(int i=0; i<rowCount; i++){
             for(int j=0; j<colCount; j++){
                 
-                boolean[][] visited = new boolean[rowCount][colCount];
-                if(helper(board, word, visited, i, j)){
+                if(helper(board, 0, word, visited, i, j)){
                     return true;
                 }
             }
@@ -16,15 +17,15 @@ class Solution {
         return false;
     }
     
-    public static boolean helper(char[][] board, String word, boolean[][] visited, int i, int j){
-        if(word.length() == 0)
+    public static boolean helper(char[][] board, int index, String word, boolean[][] visited, int i, int j){
+        if(index >= word.length())
             return true;
         
         if(i<0 || i>=board.length || j<0 || j>=board[0].length){
             return false;
         }
         
-        if(word.charAt(0) != board[i][j])
+        if(word.charAt(index) != board[i][j])
             return false;
         
         if(visited[i][j])
@@ -32,15 +33,20 @@ class Solution {
         
         visited[i][j] = true;
         
-        boolean upAns = helper(board, word.substring(1), visited, i-1, j);
-        boolean downAns = helper(board, word.substring(1), visited, i+1, j);
-        boolean leftAns = helper(board, word.substring(1), visited, i, j-1);
-        boolean rightAns = helper(board, word.substring(1), visited, i, j+1);
+        boolean upAns = helper(board, index+1, word, visited, i-1, j);
+        boolean downAns = helper(board, index+1, word, visited, i+1, j);
+        boolean leftAns = helper(board, index+1, word, visited, i, j-1);
+        boolean rightAns = helper(board, index+1, word, visited, i, j+1);
         
-        visited[i][j] = false;
+        if(upAns || downAns || leftAns || rightAns){
+            return true;
+        }
         
-        return (upAns || downAns || leftAns || rightAns);
+        visited[i][j] = false;  // if a cell is not going to give us the answer, then it must be reset to false because it may not be the starting character of word but it may be present at some other index of word. And if it will be marked as visited, then it won't be used by the call which needs it.
         
+//         Test Case which made me understand this: [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]; word = "ABCESEEE"
+        
+        return false;
     }
     
 }
