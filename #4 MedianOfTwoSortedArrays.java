cee19295c12c9cbd9e92cca57f6fb78653fdd82a
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-10-23 16:41:43 +0530

    Update #4 MedianOfTwoSortedArrays.java

--- #4 MedianOfTwoSortedArrays.java
+++ #4 MedianOfTwoSortedArrays.java
@@ -1,79 +1,75 @@
 class Solution {
-    public double findMedianSortedArrays(int[] num1, int[] num2) {
+    public double findMedianSortedArrays(int[] a, int[] b) {
         
-//         Approach-1: Naive Solution, O(m+n)
+//         Approach-1: Time = O(m+n) | space = O(m+n)
         
+//         int m = nums1.length, n = nums2.length;
+//         int[] mergedArray = new int[m + n];
+//         int g=0;
 //         int i=0, j=0;
         
-//         int m = num1.length, n = num2.length;
-        
-//         int[] num3 = new int[m+n];
-//         int g=0;
-        
-//         while(i < num1.length && j < num2.length){
-//             if(num1[i] <= num2[j]){
-//                 num3[g++] = num1[i];
+//         while(i<m && j<n){
+//             if(nums1[i] < nums2[j]){
+//                 mergedArray[g++] = nums1[i];
 //                 i++;
 //             }
 //             else{
-//                 num3[g++] = num2[j];
-//                 j++;
+//                 mergedArray[g++] = nums2[j++];
 //             }
 //         }
         
-//         while(i < num1.length){
-//             num3[g++] = num1[i];
-//             i++;
+//         while(i<m){
+//             mergedArray[g++] = nums1[i++];
 //         }
         
-//         while(j < num2.length){
-//             num3[g++] = num2[j];
-//             j++;
+//         while(j<n){
+//             mergedArray[g++] = nums2[j++];
 //         }
         
-//         if((num3.length & 1) == 0){
-//             return (double) (num3[num3.length/2] + num3[(num3.length/2) - 1]) / 2.0;
+//         if((mergedArray.length & 1) != 0){
+//             return mergedArray[(mergedArray.length-1)/2];
 //         }
-//         return num3[num3.length/2];
+        
+//         return (mergedArray[(mergedArray.length-1)/2] + mergedArray[(mergedArray.length-1)/2 + 1])/2.0;
         
         
-        
-//         Approach-2: (slightly better than the naive solution, but time complexity is same i.e. O(m+n))
-        
-        
-        int i=0, j=0;
-        
-        int m = num1.length, n = num2.length;
-        
-        int[] num3 = new int[((m+n)/2)+1];
-        int g=0;
-        
-        while(i < num1.length && j < num2.length && g < num3.length){
-            if(num1[i] <= num2[j]){
-                num3[g++] = num1[i];
-                i++;
-            }
-            else{
-                num3[g++] = num2[j];
-                j++;
-            }
-        }
-        
-        while(i < num1.length && g < num3.length){
-            num3[g++] = num1[i];
-            i++;
-        }
-        
-        while(j < num2.length && g < num3.length){
-            num3[g++] = num2[j];
-            j++;
-        }
-        
-        if(((m+n) & 1) != 0){
-            return num3[num3.length-1];
-        }
-        return (double)(num3[num3.length-1] + num3[num3.length-2])/2.0;
-        
+//         Approach-2:
+        if(a.length > b.length)
+			return findMedianSortedArrays(b, a);
+		int n1 = a.length;
+		int n2 = b.length;
+		int low = 0;
+		int high = a.length;
+		while(low <= high){
+			int cut1 = (low + high) >> 1;	//means: (low+high)/2
+			int cut2 = (n1 + n2 +1)/2 - cut1;
+			int l1 = Integer.MIN_VALUE;
+			int l2 = Integer.MIN_VALUE;
+			int r1 = Integer.MAX_VALUE;
+			int r2 = Integer.MAX_VALUE;
+			if(cut1 != 0){
+				l1 = a[cut1-1];
+			}
+			if(cut2 != 0){
+				l2 = b[cut2-1];
+			}
+			if(cut1 != n1){
+				r1 = a[cut1];
+			}
+			if(cut2 != n2){
+				r2 = b[cut2];
+			}
+			if(l1 <= r2 && l2 <= r1){
+				if((n1+n2)%2 != 0)
+					return Math.max(l1, l2);
+				return (Math.max(l1, l2) + Math.min(r1, r2))/2.0;
+			} else if(l1 > r2){
+				high = cut1 - 1;
+			} else{
+				low = cut1 + 1;
+			}
+		}
+		return 0;
         
     }
 }
