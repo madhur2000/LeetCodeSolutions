247deaecf7afc6f587c7bda679ca6acef3e39b0d
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2021-11-08 20:49:49 +0530

    Update #46 Permutations.java

--- #46 Permutations.java
+++ #46 Permutations.java
@@ -1,39 +1,58 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+//         Approach-1: Simple Recursion
+//         if(nums.length == 1){
+//             List<List<Integer>> ans = new LinkedList<>();
+//             List<Integer> list = new LinkedList<>();
+//             list.add(nums[0]);
+//             ans.add(list);
+//             return ans;
+//         }
         
-        if(nums.length == 1){
+//         List<List<Integer>> ans = new LinkedList<>();
+        
+//         for(int i=0; i<nums.length; i++){
+//             int[] a = new int[nums.length-1];
+//             int g=0;
+//             for(int j=0; j<i; j++){
+//                 a[g++] = nums[j];
+//             }
+//             for(int j=i+1; j<nums.length; j++){
+//                 a[g++] = nums[j];
+//             }
             
-            List<List<Integer>> ans = new ArrayList<>();
-            List<Integer> temp = new ArrayList<>();
-            temp.add(nums[0]);
-            ans.add(temp);
-            return ans;
-        }
+//             List<List<Integer>> sa = permute(a);
+//             for(List<Integer> list : sa){
+//                 list.add(0, nums[i]);
+//             }
+//             ans.addAll(sa);
+//         }
+//         return ans;
         
+//         Approach-2: Backtracking
+        
+        List<Integer> currPermutation = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
         
-        for(int i=0; i<nums.length; i++){
-            
-            int[] temp = new int[nums.length-1];
-            int g=0;
-            for(int j=0; j<nums.length; j++){
-                if(j != i){
-                    temp[g++] = nums[j];
-                }
-            }
-            
-            List<List<Integer>> sa = permute(temp);
-            
-            for(int k=0; k<sa.size(); k++){
-                
-                ArrayList<Integer> curr = new ArrayList<>();
-                curr.add(nums[i]);
-                curr.addAll(sa.get(k));
-                ans.add(curr);
-                
-            }
-            
-        }
+        helper(nums, ans, currPermutation, visited);
         return ans;
     }
+    
+    void helper(int[] nums, List<List<Integer>> ans, List<Integer> currList, boolean[] visited){
+        if(currList.size() == nums.length){
+            ans.add(new ArrayList(currList));
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                currList.add(nums[i]);
+                helper(nums, ans, currList, visited);
+                visited[i] = false;
+                currList.remove(currList.size()-1);
+            }
+        }
+    }
+    
 }
