4191848e3e4d595f62c0f30edaae3d42cfe1eeee
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2022-01-21 23:57:52 +0530

    Create Day-21: #134-GasStation.java

---
+++ JanuaryLeetcodingChallenge2022/Day-21: #134-GasStation.java
@@ -0,0 +1,91 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        
+//         Approach-1: gives TLE | Time: O(N^2)
+        
+        // int n = gas.length;
+        // // int currGas = 0;
+        // for(int i=0; i<n; i++){
+        //     if(gas[i] < cost[i]) continue;
+        //     int currGas = gas[i] - cost[i];
+        //     int j = (i+1)%n;
+        //     while(j != i){
+        //         currGas = currGas + gas[j];
+        //         if(currGas >= cost[j]){
+        //             currGas = currGas - cost[j];
+        //             j = (j+1)%n;
+        //         } else
+        //             break;
+        //     }
+        //     if(j == i)
+        //         return i;
+        // }
+        // return -1;
+        
+        
+        // Approach-2: Recursive | Time: O(N^2) | TLE
+        
+        
+        // int n = gas.length;
+        // for(int i=0; i<n; i++){
+        //     if(gas[i] >= cost[i] && helper(gas, cost, i, (i+1)%n, gas[i] - cost[i])){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         Approach-3: Two Pointer | O(N) | https://www.youtube.com/watch?v=zcnVaVJkLhY
+        
+        int n = gas.length;
+        
+        int front = 0;
+        int rear = 0;
+        
+        int remainingGas = 0;
+        
+        for(int i=0; i<n; i++){
+            if(gas[rear] < cost[rear]){
+                front++;
+                if(front >= n)
+                    return -1;
+                rear = front;
+            }
+            else{
+                remainingGas += gas[rear] - cost[rear];
+                rear = (rear+1) % n;
+                while(front != rear && (remainingGas + gas[rear]) >= cost[rear]){
+                    remainingGas += gas[rear] - cost[rear];
+                    rear = (rear+1) % n;
+                }
+                if(front == rear)
+                    return front;
+                if(remainingGas + gas[rear] < cost[rear]){
+                    front = rear+1;
+                    if(front >= n) return -1;
+                    rear = front;
+                    remainingGas = 0;
+                }
+            }
+        }
+        return -1;
+    }
+    
+//     public static boolean helper(int[] gas, int[] cost, int start, int index, int remainingGas){
+        
+//         if(index == start) return true;
+        
+//         if(remainingGas + gas[index] >= cost[index]){
+//             return helper(gas, cost, start, (index+1)%gas.length, remainingGas + gas[index] - cost[index]);
+//         }
+//         return false;
+//     }
+}
