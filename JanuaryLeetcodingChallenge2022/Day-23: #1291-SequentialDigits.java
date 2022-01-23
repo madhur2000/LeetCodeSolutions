0c04cee0e3dd15de09b8987769def057aa9d0bbd
Madhur Rakheja <55992831+madhur2000@users.noreply.github.com>
2022-01-23 23:35:19 +0530

    Create Day-23: #1291-SequentialDigits.java

---
+++ JanuaryLeetcodingChallenge2022/Day-23: #1291-SequentialDigits.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        // List<Integer> ans = new ArrayList<>();
+        // for(int i=low; i<=high; i++){
+        //     if(isSequential(i)){
+        //         ans.add(i);
+        //         // ans.addAll(sequentialDigits(i+1, high));
+        //         // return ans;
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+        
+        int[] allNums = {12,23,34,45,56,67,78,89,
+                         123,234,345,456,567,678,789,
+                         1234,2345,3456,4567,5678,6789,
+                         12345,23456,34567,45678,56789,
+                         123456,234567,345678,456789,
+                         1234567,2345678,3456789,
+                         12345678,23456789,
+                         123456789};
+        List<Integer> res = new ArrayList<>();
+        int n = allNums.length;
+        for (int i = 0; i < n; i++) {
+            if (allNums[i] < low) continue;
+            if (allNums[i] > high) break;
+            res.add(allNums[i]);
+        }
+        return res;
+    }
+    
+    // boolean isSequential(int num){
+    //     // int prev_digit = num%10;
+    //     // num/=10;
+    //     // int curr_digit = num%10;
+    //     // num/=10;
+    //     int prev_digit = num%10;
+    //     num/=10;
+    //     while(num != 0){
+    //         int curr_digit = num%10;
+    //         if(prev_digit != curr_digit+1)
+    //             return false;
+    //         prev_digit = curr_digit;
+    //         num/=10;
+    //     }
+    //     return true;
+    // }
+}
